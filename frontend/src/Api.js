da654e6b85e52d6469378b97f530ae8d23134fda
Toby <woollumst@gmail.com>
2025-01-10 20:36:18 -0700

    Update Api.js

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,1 +1,9 @@
- 
+const API_URL = "http://localhost:8080/employee";
+
+export const fetchEmployees = async () => {
+  const response = await fetch('${API_URL}');
+  if (!response.ok) {
+    throw new Error('Failed to fetch Employees: ${response.statusText}');
+  }
+  return response.json();
+};
