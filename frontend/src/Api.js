76786f13afabcb21128f4b417f4d028cd9b9b931
Toby <woollumst@gmail.com>
2025-01-10 20:51:53 -0700

    Update Api.js

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:8080/employee";
+const API_URL = "http://localhost:8080/Employee";
 
 export const fetchEmployees = async () => {
   const response = await fetch('${API_URL}');
