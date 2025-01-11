b3434259c166f428d025b96347a69916c55abae8
Toby <woollumst@gmail.com>
2025-01-10 18:28:30 -0700

    Update EmployeeList.js

--- frontend/src/EmployeeList.js
+++ frontend/src/EmployeeList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 function EmployeeList(){
-    const employees = ["Jonny Bravo", "John Smith", "Jane Doe"];
-
+    //const employees = ["Jonny Bravo", "John Smith", "Jane Doe"];
     return(
         <div>
-            
+            <h2>Employee List</h2>
+            //list employees here
         </div>
-    )
+    );
 }
+
+export default EmployeeList;
