7dbbe5873a948bffe5a7cb51833dd66eb7c6e209
Toby <woollumst@gmail.com>
2025-01-10 22:04:48 -0700

    Update EmployeeItem.js

--- frontend/src/EmployeeItem.js
+++ frontend/src/EmployeeItem.js
@@ -1,8 +1,16 @@
 import {getEmployee} from './Api.js';
 
-function EmployeeItem() {
+function EmployeeItem({employeeID}) {
   return (
-    
+    {id, name, phone, salary, address} = getEmployee({employeeID});
+    <div>
+      <h2>Name: {name}</h2>
+      <p>ID: {id}</p>
+      <p>Phone: {phone}</p>
+      <p>Salary: {salary}</p>
+      <p>Address: {address}</p>
+      <button>Delete Employee(TODO)</button>
+    </div>
   );
 }
 
